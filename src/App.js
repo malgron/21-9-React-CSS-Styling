b75cdc43b8d9c2874c59414416c42894e6cebb3b
Daniel Anaya <malgron@hotmail.com>
2022-10-19 09:37:22 -0600

    Assessment 21.9

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+
+  
+  return (
+    <div>
+      <h1 style={{textAlign: "center", fontFamily: "cursive"}}>Hello!</h1>
+      <h4 style={{textAlign: "center", fontFamily: "monospace"}}>How are you?</h4>
+      <h5 className="App-weather-message" style={{textAlign: "center"}}>Today is a nice day!</h5>
+    </div>
+  );
+}
+
+export default App;
